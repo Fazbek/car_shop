66e1ae217aa3743c94f3805ca4799de49c668f64
Fazbek <bekzeon7717@gmail.com>
2024-06-09 15:11:57 +0300

    Car_project

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///social_network.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///car_shop.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False})
 
